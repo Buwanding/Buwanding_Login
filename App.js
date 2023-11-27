9e1a0601e0bd6f9971192f90f51d8ecb318baad4
Buwanding <buwandingaladdin@gmail.com>
2023-11-27 21:06:54 +0800

    iniliatize

--- App.js
+++ App.js
@@ -1,9 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-
+import Login from './screens/Login';
+import Register from './screens/Register';
 export default function App() {
   return (
     <View style={styles.container}>
+      <Login/>
+      <Register/>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
---
+++ screens/Login.js
@@ -0,0 +1,16 @@
+import { StyleSheet, Text, View,TextInput, Button} from 'react-native'
+import React from 'react'
+
+const Login = () => {
+  return (
+    <View>
+      <Text>Login</Text>
+      <TextInput></TextInput>
+      <Button></Button>
+    </View>
+  )
+}
+
+export default Login
+
+const styles = StyleSheet.create({})
